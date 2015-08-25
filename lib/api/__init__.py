0f908dab8e9d02719c1a65926b326e7008782076
Francesco d'Aloisio <francesco.daloisio@serco.com>
2015-08-25 17:40:47 +0200

    added detection api

--- lib/api/__init__.py
+++ lib/api/__init__.py
@@ -2,14 +2,13 @@ import json,threading
 from lib.log import logger
 import lib.util as active
 from flask import Flask,request
-from flask.ext.cors import CORS
+from flask_cors import CORS
 
 
 def routing(app):
 
     @app.route('/api', methods=['GET'])
     def hello_word():
-
         logger.info("Received %s" %(request.url))
         return
 
@@ -24,11 +23,22 @@ def routing(app):
         logger.info("Received %s" %(request.url))
         active.setActive(newStatus)
         return json.dumps(active.getActive())
+    @app.route('/api/setDetection', methods=['GET'])
 
+    def setDetection():
+        newStatus = request.args.get('status')
+        logger.info("Received %s" %(request.url))
+        active.setDetection(newStatus)
+        return json.dumps(active.getDetection())
+
+    @app.route('/api/getDetection', methods=['GET'])
+    def getDetection():
+        logger.info("Received %s" %(request.url))
+        return json.dumps(active.getDetection())
 
 def createApp():
     app = Flask(__name__)
-    cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+    CORS(app, resources={r"/api/*": {"origins": "*"}})
     routing(app)
     app.run('0.0.0.0')
 
